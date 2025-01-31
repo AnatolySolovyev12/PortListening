3df27cf3e1078f242f7aeade4d6a873695e5a1c7
Анатолий Соловьев <bakalavr12@mail.ru>
2025-01-31 15:53:47 +0500

    Add tray function

--- PortListening/main.cpp
+++ PortListening/main.cpp
@@ -1,15 +1,41 @@
-#include <QCoreApplication>
+
 #include "mytcpserver.h"
 #include <QTextStream>
 #include <qfile.h>
+
+
+#include <QApplication>
+#include <QSystemTrayIcon>
+#include <QMenu>
+#include <QAction>
 
 QTextStream myout(stdout);
 
 int main(int argc, char* argv[])
 {
-    QCoreApplication a(argc, argv);
 
-    MyTcpServer server;
-    
-    return a.exec();
+
+	QApplication app(argc, argv);
+
+	MyTcpServer server;
+
+
+	QSystemTrayIcon trayIcon; 
+	trayIcon.setIcon(QIcon("icon.png")); 
+	trayIcon.setVisible(true);
+
+	// Создаем контекстное меню для иконки
+	QMenu menu;
+	QAction* quitAction = menu.addAction("Exit");
+	QObject::connect(quitAction, &QAction::triggered, &server, &QApplication::quit);
+
+	trayIcon.setContextMenu(&menu);
+
+	// Скрываем главное окно
+	// Если у вас есть главное окно, вы можете его скрыть
+	// mainWindow.hide();
+
+
+
+	return app.exec();
 }
