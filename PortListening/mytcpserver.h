85a377ea01a5b79264736a22378c301c5fa586ab
AnatolySolovyev12 <167902367+AnatolySolovyev12@users.noreply.github.com>
2025-06-21 08:32:03 +0300

    ohhh

--- PortListening/mytcpserver.h
+++ PortListening/mytcpserver.h
@@ -48,8 +48,8 @@ private:
     bool oldMessege = false;
 
     QString answerListMilur;
-    QString zeroBuff = QString(108, '0');
-    QString strZero = QString(52, '0');
+    QString zeroBuff = QString(98, '0');
+    QString strZero = QString(62, '0');
 
     QList<QByteArray>serialBuff;
 };
